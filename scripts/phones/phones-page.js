b47ebecb573bed67ba7483fd71bcfe50028d2a25
Aleksandr Sesyolkin <sesyolkin@gmail.com>
2018-12-27 21:15:53 +0300

    Add method selectionSearchResults for the search component

--- scripts/phones/phones-page.js
+++ scripts/phones/phones-page.js
@@ -29,6 +29,46 @@ export default class PhonesPage {
         element: this._element.querySelector('[data-component="phones-catalog"]'),
         phones: phones
       })
+
+      selectionSearchResults.call(this._catalogue);
+
+      function selectionSearchResults() {
+        let allHTMLforSearch = this._element.children[0].innerHTML.toLowerCase().trim();
+        let allHTMLforInsert = this._element.children[0].innerHTML.trim();
+        
+        outer: for (let i = 0; i < allHTMLforSearch.length; ++i) {
+          
+          if (allHTMLforSearch[i] === '<') {
+            i = allHTMLforSearch.indexOf('>', i);
+            continue outer;
+          };
+
+          if (allHTMLforSearch[i] === searchQuery[0]) {
+            let a = i;
+            let b = 0;
+            for (; b < searchQuery.length;) {
+              if (allHTMLforSearch[a] === searchQuery[b]) {
+                ++a;
+                ++b;
+              }
+              else {
+                i = a;
+                continue outer;
+              }
+            }
+
+            let newPartValue = '<span class="matches">' + allHTMLforInsert.slice(i, i + searchQuery.length) + '</span>';
+
+            allHTMLforSearch = allHTMLforSearch.slice(0, i) + newPartValue + allHTMLforSearch.slice(i + searchQuery.length);
+
+            allHTMLforInsert = allHTMLforInsert.slice(0, i) + newPartValue + allHTMLforInsert.slice(i + searchQuery.length);
+
+            i += newPartValue.length - 1;
+          }
+        }
+
+        this._element.children[0].innerHTML = allHTMLforInsert;
+      };
     });
   }
 }
