6e78a969d3350258c1fd1bd98c2d61d38bd87893
Aleksandr Sesyolkin <sesyolkin@gmail.com>
2019-01-20 18:55:30 +0300

    Added a button to remove goods from the basket

--- scripts/phones/components/basket.js
+++ scripts/phones/components/basket.js
@@ -9,19 +9,46 @@ export default class Basket extends Component {
     this._listPhonesContainer = listPhonesContainer;
 
     this._render();
+
+    this._element.addEventListener('click', this._removeButton);
   }
 
   addToBasket(event) {
     let listOfGoods = this._element.querySelector('ul');
     let phoneName = event.detail;
+    let removeButton = document.createElement('span');
+
+    removeButton.textContent = 'X';
+    removeButton.dataset.element = 'remove-button';
 
     if (listOfGoods.children[0].textContent === 'нет товаров') {
       listOfGoods.children[0].textContent = phoneName;
+      listOfGoods.children[0].classList.add('product');
+      listOfGoods.children[0].appendChild(removeButton);
     } else {
-      let newGoods = document.createElement('li');
-      newGoods.textContent = phoneName;
-      listOfGoods.appendChild(newGoods);
+      let newProduct = document.createElement('li');
+      newProduct.textContent = phoneName;
+      newProduct.classList.add('product');
+      listOfGoods.appendChild(newProduct);
+      newProduct.appendChild(removeButton);
     }
+  }
+
+  _removeButton(event) {
+    let removeButton = event.target.closest('[data-element="remove-button"]');
+
+    if (!removeButton) {
+      return;
+    }
+    
+    let listOfGoods = removeButton.closest('ul');
+
+    if (listOfGoods.children.length === 1) {
+      listOfGoods.children[0].textContent = 'нет товаров';
+    } else {
+      removeButton.parentElement.remove();
+    }
+
   }
 
   _render() {
