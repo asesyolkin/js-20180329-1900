92f7cf3ece096905aac09da318e7cac84e7c5996
Aleksandr Sesyolkin <sesyolkin@gmail.com>
2019-01-22 12:39:42 +0300

    Downloading phones from github instead of local server

--- scripts/phones/services/phones-service.js
+++ scripts/phones/services/phones-service.js
@@ -1,18 +1,21 @@
 'use strict';
 
+let BASE_API_URL = 'https://asesyolkin.github.io/js-20180329-1900/api';
+
 const PhonesService = {
   loadPhones(callback) {
-    this._sendRequest('/api/phones', callback);
+    this._sendRequest('/phones', callback);
   },
 
   loadPhone(phoneId, callback) {
-    this._sendRequest(`/api/phones/${ phoneId }`, callback);
+    this._sendRequest(`/phones/${ phoneId }`, callback);
   },
 
   _sendRequest(url, callback, { method = 'GET' } = {}) {
     let xhr = new XMLHttpRequest();
+    let fullUrl = BASE_API_URL + url + '.json';
 
-    xhr.open(method, url, true);
+    xhr.open(method, fullUrl, true);
 
     xhr.send();
 
